fd16c4af25ba023a2727d23f9ad05ac0a71e3987
Eileen McNaughton <eileen@fuzion.co.nz>
2015-05-19 00:34:12 +1200

    comment improvement

--- src/Message/SystemCompleteAuthorizeResponse.php
+++ src/Message/SystemCompleteAuthorizeResponse.php
@@ -66,9 +66,9 @@ class SystemCompleteAuthorizeResponse extends AbstractResponse
      * Verifies the validity of the signature.
      *
      * Function adapted from LexikPayboxBundle.
-     *
      * @return bool
-     * @throws InvalidRequestException
+     * @throws \Omnipay\Common\Exception\InvalidResponseException
+     * @throws \Omnipay\Paybox\Message\InvalidRequestException
      */
     public function verifySignature()
     {
